8b4acca4c46bc0661c07a2a37c0c88e3d4ee4662
Alamgir Qazi <alamgirmunirqazi@gmail.com>
2019-08-24 21:17:32 +0500

    added live URL

--- frontend/src/sdk/base.config.ts
+++ frontend/src/sdk/base.config.ts
@@ -2,7 +2,7 @@ import { environment } from '../environments/environment';
 
 export class Baseconfig {
   // private static path = 'http://115.186.156.98:3000';
-  private static path = 'http://localhost:3000';
+  private static path = 'https://pmis-backend-now-v-bqpltvbzfz.now.sh';
   // private static path = 'http://192.168.0.161:3000';
   private static api_version = '1';
   public static getPath(): string {
